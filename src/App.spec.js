6f7bd00a66a81c9e03b536bf1448b4e4121b169d
Matteo Pietro Dazzi <matteo.dazzi@mia-platform.eu>
2022-08-31 19:48:01 +0200

    chore: lint

--- src/App.spec.js
+++ src/App.spec.js
@@ -3,8 +3,8 @@ import {render} from '@testing-library/vue'
 
 import App from './App.vue'
 
-describe("App", () => {
-    it("renders correctly", () => {
+describe('App', () => {
+    it('renders correctly', () => {
         const {getByText} = render(App)
 
         expect(getByText('Click on the Vite and Vue logos to learn more')).toBeDefined()
