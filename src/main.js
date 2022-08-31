060a02ee31d9783fd5a5df56e9b67f1b71313eca
Matteo Pietro Dazzi <matteo.dazzi@mia-platform.eu>
2022-08-31 22:20:24 +0200

    chore: lint

--- src/main.js
+++ src/main.js
@@ -1,29 +1,32 @@
-import {createApp} from 'vue'
-import {renderWithQiankun, qiankunWindow} from 'vite-plugin-qiankun/dist/helper'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import {
+  renderWithQiankun,
+  qiankunWindow,
+} from "vite-plugin-qiankun/dist/helper";
+import "./style.css";
+import App from "./App.vue";
 
-let root
+let root;
 
-const rootSelector = '#app'
+const rootSelector = "#app";
 
-const render = ({container} = {}) => {
-    root = createApp(App)
-    root.mount(container ? container.querySelector(rootSelector) : rootSelector)
-}
+const render = ({ container } = {}) => {
+  root = createApp(App);
+  root.mount(container ? container.querySelector(rootSelector) : rootSelector);
+};
 
 renderWithQiankun({
-    mount(props) {
-        render(props)
-    },
-    bootstrap() { },
-    unmount() {
-        root.unmount()
-        root._container.innerHTML = ''
-        root = null
-    },
-})
+  mount(props) {
+    render(props);
+  },
+  bootstrap() {},
+  unmount() {
+    root.unmount();
+    root._container.innerHTML = "";
+    root = null;
+  },
+});
 
 if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
-    render({})
+  render({});
 }
